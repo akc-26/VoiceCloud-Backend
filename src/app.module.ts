ee709407d3f2c0636b49ef78a30548def8fb6e13
BenPatrick2026 <benpatrick2989@gmail.com>
2026-07-10 20:56:09 +0530

    Phase 1B: Backend module skeleton structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,19 @@ import { RedisModule } from './redis/redis.module';
 import { HealthModule } from './modules/health/health.module';
 import { AppLogger } from './common/logger/app-logger.service';
 
+// Phase 1B Modules Skeletons
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
+import { RoomsModule } from './modules/rooms/rooms.module';
+import { ChatModule } from './modules/chat/chat.module';
+import { WalletModule } from './modules/wallet/wallet.module';
+import { GiftsModule } from './modules/gifts/gifts.module';
+import { VipModule } from './modules/vip/vip.module';
+import { HostsModule } from './modules/hosts/hosts.module';
+import { AgenciesModule } from './modules/agencies/agencies.module';
+import { RankingsModule } from './modules/rankings/rankings.module';
+import { NotificationsModule } from './modules/notifications/notifications.module';
+
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -17,6 +30,17 @@ import { AppLogger } from './common/logger/app-logger.service';
     DatabaseModule,
     RedisModule,
     HealthModule,
+    AuthModule,
+    UsersModule,
+    RoomsModule,
+    ChatModule,
+    WalletModule,
+    GiftsModule,
+    VipModule,
+    HostsModule,
+    AgenciesModule,
+    RankingsModule,
+    NotificationsModule,
   ],
   providers: [AppLogger],
 })
